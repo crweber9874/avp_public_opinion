c09d3bf55fff44134ef654428ff8e15c99dc9e81
Christopher Weber <chrisweber@email.arizona.edu>
2024-10-21 10:17:59 -0700

    # Gun control

--- shiny/guns/global.R
+++ shiny/guns/global.R
@@ -9,3 +9,8 @@ load("data_guns.rda")
 
 dat = df
 full_dat = df
+
+
+dat$age_cohort <- ifelse(dat$age_cohort=="30-44", "30-44", dat$age_cohort)
+df$age_cohort <- ifelse(df$age_cohort=="45-64", "45-64", df$age_cohort)
+dat_guns$age_cohort <- ifelse(dat_guns$age_cohort=="45-64", "45-64", dat_guns$age_cohort)
